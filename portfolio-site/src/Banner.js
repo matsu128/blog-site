124826f5e0167de57f50dfbf1528b319c3391810
matsumoto <soumatsumoto1282002@icloud.com>
2023-09-05 22:11:28 +0900

    Banner.jsの内容変更

--- portfolio-site/src/Banner.js
+++ portfolio-site/src/Banner.js
@@ -6,6 +6,10 @@ const Banner = () => {
     <div className="banner" id="banner">
       <h1>Welcome to My Portfolio</h1>
       <p>Hello, I'm John Doe. I'm a Web Developer.</p>
+      <div className="career">
+        <h2>未経験からバックエンドエンジニアとしての現在に至るまでの軌跡</h2>
+        <p>使用した言語etc...</p>
+      </div>
     </div>
   );
 };
