5b4f44424a2fa47743c28866dcf015fa265b7cbe
matsumoto <soumatsumoto1282002@icloud.com>
2024-06-08 11:46:57 +0900

    feat: Add MyComponent and submitName in the pages folder, and remove backend directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 /** @format */
 
 import './App.css';
+import Index from './pages/index';
 
 function App() {
   return (
     <>
       <div>
         <h2>Hello World</h2>
+        <Index />
       </div>
     </>
   );
---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,15 @@
+// src/pages/index.jsx
+
+import React from 'react';
+import HelloComponent from '../components/MyComponent';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <HelloComponent />
+    </div>
+  );
+};
+
+export default HomePage;
