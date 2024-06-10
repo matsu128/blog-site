6a8070866731d015b8f7ea4ba53d4d4beb88ad37
philipuuu <topgun0415@naver.com>
2024-06-10 13:10:23 +0900

    Restore app folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import './App.css';
+import Index from './pages/index';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>Hello World</h2>
+        <Index />
+      </div>
+    </>
+  );
+}
+
+export default App;
