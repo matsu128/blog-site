d8e5d44466e47713d6e2fa30c0511356b60e9d44
matsumoto <soumatsumoto1282002@icloud.com>
2024-06-11 22:35:09 +0900

    Added database interaction in login.js

--- blog-site/pages/api/login.js
+++ blog-site/pages/api/login.js
@@ -1,18 +1,34 @@
-// pages/api/login.js
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-      const { password } = req.body;
-      
-      console.log('login.jsのif post送信pass.pass = ' + password + ' である')
-      // メールアドレスとパスワードが空でないか確認
-      if (password) {
-        console.log('login.jsの空白チェックpass')
-        res.status(200).json({ message: 'Email and password are required' });
-        return;
+import { getUserByUsernameAndPassword } from '../../utils/db';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name, password } = req.body;
+
+    try {
+      // データベースからユーザー情報を取得
+      const user = await getUserByUsernameAndPassword(name, password);
+
+      if (user) {
+        // ユーザーが存在する場合、必要な情報を返す
+        const userData = {
+          name: user.name,
+          title: user.title,
+          content: user.content,
+          imageUrl: user.imageUrl,
+          postDate: user.postDate,
+          userId: user.userId,
+        };
+        res.status(200).json(userData);
+      } else {
+        // ユーザーが存在しない場合はエラーを返す
+        res.status(401).json({ message: 'Invalid name or password' });
       }
-    } else {
-      res.setHeader('Allow', ['POST']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
     }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-  
+}
