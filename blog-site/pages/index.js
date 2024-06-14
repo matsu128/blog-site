2784244bd82cca4a1d80894dc9ba139120f103a3
philipuuu <topgun0415@naver.com>
2024-06-15 00:46:01 +0900

    move : layout pages moved to pages directory for page router

--- blog-site/pages/index.js
+++ blog-site/pages/index.js
@@ -2,5 +2,12 @@
 import LoginPage from '../src/login/LoginPage';
 
 export default function Home() {
-  return <LoginPage />;
+  return (
+    <>
+      <div>
+        <title>Welcome to myBlog</title>
+        <LoginPage />
+      </div>
+    </>
+  );
 }
---
+++ blog-site/pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginPage from '../src/login/LoginPage';
+
+const login = () => {
+  return (
+    <>
+      <title>Login</title>
+      <LoginPage />
+    </>
+  );
+};
+
+export default login;
