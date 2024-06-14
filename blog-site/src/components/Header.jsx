79b862b8076b73f2d68cd9a465c79acce4689dee
philipuuu <topgun0415@naver.com>
2024-06-15 00:47:12 +0900

    Add : Header components (not finished)

---
+++ blog-site/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <p className='text-sm font-medium text-red-900'>hello</p>
+    </>
+  );
+};
+
+export default Header;
