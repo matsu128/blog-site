afa5615314ddddde680382215cc32f8fb5e9cff6
matsumoto <soumatsumoto1282002@icloud.com>
2024-07-15 14:18:43 +0900

    style: Update pagination and new post button design on list page for responsiveness
    - Adjusted layout to ensure pagination and new post button are responsive.
    - Added margins to the sides on mobile screens for better spacing.

--- src/list/BlogList.jsx
+++ src/list/BlogList.jsx
@@ -9,16 +9,17 @@ const BlogList = ({ articles, currentPage, totalPages, onPageChange, onSelectArt
       <div className="pt-20">
         <ArticleItem articles={articles} onSelect={onSelectArticle} />
       </div>
-      <div className="mt-10 pb-10 flex justify-between items-center">
-        <div className="w-full md:w-7/12 flex justify-end">
-          <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange} />
-        </div>
-        {userId && (
-          <div onClick={onNewPost} className="w-full md:w-3/12 ml-auto pr-10 md:w-40">
-            <Button text="New Post" />
-          </div>
-        )}
-      </div>
+      <div className="mt-10 pb-10 flex flex-col md:flex-row justify-between items-center">
+  <div className="w-full md:w-7/12 mb-4 md:mb-0 md:flex md:justify-end">
+    <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange} />
+  </div>
+  {userId && (
+    <div onClick={onNewPost} className="md:w-3/12 mx-auto md:ml-auto md:pr-10 md:w-40 sm:mx-5">
+      <Button text="New Post" />
+    </div>
+  )}
+</div>
+
     </div>
   );
 };
